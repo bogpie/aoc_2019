0ee794d87e1c80f78bb38ff0fdd2add8c9e8e237
Bogdan Piele <bogdanpielemail@gmail.com>
2019-12-01 07:17:02 +0200

    day 1

---
+++ d01/d01.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+
+using namespace std;
+
+ifstream fin ("d01.in");
+ofstream fout("d01.out");
+
+void part1 (int mass,int &fuelReq)
+{
+    fuelReq = mass / 3 - 2;
+}
+
+void part2 (int mass,int &fuelReq)
+{
+    while (mass>0)
+    {
+        mass = mass / 3 - 2;
+        if (mass < 0) mass = 0;
+        fuelReq += mass;
+    }
+}
+
+int main ()
+{
+    unsigned long long sum = 0;
+
+    //while (!fin.eof())
+
+    int mass;
+    while (fin >> mass)
+    {
+        int fuelReq = 0 ;
+        ///part1 (mass,fuelReq);
+        part2 (mass,fuelReq);
+        sum += fuelReq;
+    }
+
+    fout << sum;
+}
